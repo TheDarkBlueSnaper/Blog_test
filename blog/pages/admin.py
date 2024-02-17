f9cac79e897595acb3226cbe8bd8f1a765066d23
TheDarkBlueSnaper <mr.e.gegeckas@gmail.com>
2024-02-17 22:50:37 +0200

    alpha 0.3

--- blog/pages/admin.py
+++ blog/pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Article, Category
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'owner', 'category')
+    list_filter = ('date', 'category')
+    search_fields = ('title', 'owner__username', 'category__name')
+    date_hierarchy = 'date'
+    ordering = ('-date',)
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category)
